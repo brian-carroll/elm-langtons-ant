162e51ccfefcad298510e57698874fd3b7d76110
Brian Carroll <brian.carroll.ireland@gmail.com>
2017-03-09 00:27:45 +0000

    Code cosmetics

--- src/Types.elm
+++ src/Types.elm
@@ -7,13 +7,9 @@ type alias Position =
     ( Int, Int )
 
 
+-- List of black squares to draw (just don't draw the white ones)
 type alias Board =
     List Position
-
-
-type Square
-    = Black
-    | White
 
 
 type Direction
